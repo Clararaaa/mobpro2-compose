4ce387ad660e7509b15eb0e28053106605100686
Clararaaa <clara01angelica@gmail.com>
2026-02-12 11:20:24 +0700

    Membuat Menu Logout

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.ui.auth)
+    implementation(libs.compose.material.icons.extended)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
