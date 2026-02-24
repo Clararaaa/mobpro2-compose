8feb5ea128c2e086e2fcb73b63813b532c5ad886
Clararaaa <clara01angelica@gmail.com>
2026-02-24 22:02:48 +0700

    Mengambil data kelas dari database

--- app/src/main/java/com/indraazimi/mobpro2/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/indraazimi/mobpro2/ui/screen/main/MainScreen.kt
@@ -9,15 +9,22 @@
 
 package com.indraazimi.mobpro2.ui.screen.main
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -27,8 +34,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.firebase.auth.FirebaseUser
-import com.indraazimi.mobpro2s.ui.AppBarWithLogout
 import com.indraazimi.mobpro2.R
+import com.indraazimi.mobpro2s.ui.AppBarWithLogout
 import com.indraazimi.mobpro2s.ui.UserProfileCard
 
 @Composable
@@ -38,6 +45,10 @@ fun MainScreen(
     val viewModel: MainViewModel = viewModel()
 
     var showDialog by remember { mutableStateOf(false) }
+
+    LaunchedEffect(user.uid) {
+        viewModel.loadData(user.uid)
+    }
 
     Scaffold(
         topBar = { AppBarWithLogout(R.string.app_name) },
@@ -51,12 +62,30 @@ fun MainScreen(
             }
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .padding(16.dp)
+        LazyColumn(
+            modifier = Modifier.padding(innerPadding),
+            contentPadding = PaddingValues(bottom = 84.dp)
         ) {
-            UserProfileCard(user)
+            item {
+                UserProfileCard(
+                    user = user,
+                    modifier = Modifier.padding(16.dp)
+                )
+                HorizontalDivider(
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+            }
+            items(viewModel.data) {
+                Card(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+                ) {
+                    Text(
+                        text = it.nama,
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp)
+                    )
+                }
+            }
         }
 
         if (showDialog) {
