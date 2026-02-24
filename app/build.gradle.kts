3748eb37273f5c78fdd5460f1c7b7482fd3c3af4
Clararaaa <clara01angelica@gmail.com>
2026-02-24 19:43:17 +0700

    Membuat dialog tambah kelas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.compose.material.icons.extended)
     implementation(project(":shared"))
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.ui.auth)
